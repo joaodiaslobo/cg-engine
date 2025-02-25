62fc86408f825e2973eeb34c2ff9e1f988713f6a
Afonso-santos <afonso.ds33@gmail.com>
2025-02-25 12:12:14 +0000

    fix:Centralisation of the plane

--- generator/src/Generator.cpp
+++ generator/src/Generator.cpp
@@ -58,19 +58,26 @@ Model Plane(float length, int divisions) {
   vector<vec3> vertices;
 
   float divisionSize = length / divisions;
+  float halfLength = length / 2.0f;
 
   for (int x = 0; x < divisions; x++) {
     for (int z = 0; z < divisions; z++) {
-      vec3 bottomLeft = vec3(x * divisionSize, 0, z * divisionSize);
-      vec3 bottomRight = vec3((x + 1) * divisionSize, 0, z * divisionSize);
-      vec3 topLeft = vec3(x * divisionSize, 0, (z + 1) * divisionSize);
-      vec3 topRight = vec3((x + 1) * divisionSize, 0, (z + 1) * divisionSize);
+      // Offset x and z so that the plane is centered at the origin
+      float xPos = (x * divisionSize) - halfLength;
+      float zPos = (z * divisionSize) - halfLength;
+      float xNext = ((x + 1) * divisionSize) - halfLength;
+      float zNext = ((z + 1) * divisionSize) - halfLength;
 
+      vec3 bottomLeft = vec3(xPos, 0, zPos);
+      vec3 bottomRight = vec3(xNext, 0, zPos);
+      vec3 topLeft = vec3(xPos, 0, zNext);
+      vec3 topRight = vec3(xNext, 0, zNext);
+
+      // triangles
       vertices.insert(vertices.end(), {topLeft, bottomLeft, bottomRight});
       vertices.insert(vertices.end(), {topLeft, bottomRight, topRight});
     }
   }
-
   return {vertices};
 }
 
