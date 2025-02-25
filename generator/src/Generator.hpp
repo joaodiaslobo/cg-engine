77169583ca759ec8bca75cee3cab4d9990dde0ff
Rita Camacho <ritacunhacamacho@gmail.com>
2025-02-25 19:24:32 +0000

    feat: sphere generation

--- generator/src/Generator.hpp
+++ generator/src/Generator.hpp
@@ -10,5 +10,6 @@ struct Model {
 
 namespace generator {
 Model Cone(float radius, float height, int slices, int stacks);
+Model Sphere(float radius, int slices, int stacks);
 bool Export(const Model& model, const std::string& filename);
 }  // namespace generator
